b7a28e3afbbeda8bf2dd5b2eb83c8cabdb6741ad
Armen123-alt <abrahamyan0310@gmail.com>
2026-03-27 15:26:13 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,505 @@
+function vb(){
+   document.getElementById("krd1").style = " color:rgba(193, 70, 121, 0.2);";
+    document.getElementById("jjk2").style = " color:#8e2d55"
+      document.getElementById("jjk").style = " color:black;";
+  document.getElementById("arm711id").style = " color:rgba(21, 21, 21, 0.41);"
+     document.getElementById("jjk1").style = " color: rgba(21, 21, 21, 0.41);"
+      document.getElementById("krd2").style = " color: black;";
+       document.getElementById("qwerty").innerHTML =
+    "providing quality baked goods for all customers";
+  document.getElementById("qwerty1").innerHTML =
+    "Our mission is to create a bakery that makes the best quality baked goods on site from scratch, and where both employees and customers would feel comfortable.";
+}
+
+
+
+
+
+
+
+
+
+function h() {
+  document.getElementById("jjk").style = " color: #8e2d55;";
+  document.getElementById("arm711id").style = " color:rgba(193, 70, 121, 0.2);";
+  document.getElementById("krd2").style = " color: black;";
+   document.getElementById("jjk1").style = " color: rgba(21, 21, 21, 0.41);";
+    document.getElementById("jjk2").style = " color: black;";
+  document.getElementById("krd1").style = " color:gray;";
+  document.getElementById("qwerty").innerHTML =
+    "Ensuring the Best Atmosphere for Everyone";
+  document.getElementById("qwerty1").innerHTML =
+    "We see the most important part of our business in ensuring the happiness of our staff and the satisfaction of our clients by creating a welcoming and caring atmosphere";
+}
+function ypo() {
+   document.getElementById("krd2").style = " color: #8e2d55;";
+   document.getElementById("jjk1").style = " color:rgba(193, 70, 121, 0.2);"
+    document.getElementById("jjk2").style = " color:black;"
+     document.getElementById("krd1").style = " color:rgba(21, 21, 21, 0.41);"
+      document.getElementById("jjk").style = " color: black;";
+  document.getElementById("arm711id").style = " color:rgba(21, 21, 21, 0.41)"
+  document.getElementById("qwerty").innerHTML =
+    "Serving only the freshest baked goods for you";
+  document.getElementById("qwerty1").innerHTML =
+    "We aim to deliver the best baked goods for corporate events and individual celebrations, while also offering the best level of customer service in the United States.";
+}
+let r=false
+function lg(){
+  if(r==false){
+      document.getElementById("armjsid").style.transform="scale(1)"
+  document.getElementById("jk").className="fa fa-times"
+
+  }
+  else{
+     document.getElementById("armjsid").style.transform="scale(0)"
+  document.getElementById("jk").className="fa fa-shopping-cart"
+  }
+
+r=!r
+}
+
+function ty3(){
+
+  let a= document.getElementById("id1");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id2");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty4(){
+
+
+   let a= document.getElementById("id1");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id2");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+function ty33(){
+  let a= document.getElementById("id11");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id21");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty44(){
+
+
+   let a= document.getElementById("id11");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id21");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+
+
+let x=false
+function lg1(){
+  if(x==false){
+      document.getElementById("armjsida").style.transform="scale(1)"
+  document.getElementById("jk1").className="fa fa-times"
+
+  }
+  else{
+     document.getElementById("armjsida").style.transform="scale(0)"
+  document.getElementById("jk1").className="fa fa-shopping-cart"
+  }
+
+x=!x
+}
+let rt=false
+function lg2(){
+  if(rt==false){
+      document.getElementById("armjsidad").style.transform="scale(1)"
+  document.getElementById("jk11").className="fa fa-times"
+
+  }
+  else{
+     document.getElementById("armjsidad").style.transform="scale(0)"
+  document.getElementById("jk11").className="fa fa-shopping-cart"
+  }
+
+rt=!rt
+}
+
+
+
+
+let ik=false
+function lg3(){
+  if(ik==false){
+      document.getElementById("armjsidadf").style.transform="scale(1)"
+  document.getElementById("jk111").className="fa fa-times"
+
+  }
+  else{
+     document.getElementById("armjsidadf").style.transform="scale(0)"
+  document.getElementById("jk111").className="fa fa-shopping-cart"
+  }
+
+ik=!ik
+}
+
+
+
+let rt1=false
+function lg4(){
+  if(rt1==false){
+      document.getElementById("arm21idadm").style.transform="scale(1)"
+  document.getElementById("jk11m").className="fa fa-times"
+
+  }
+  else{
+     document.getElementById("arm21idadm").style.transform="scale(0)"
+  document.getElementById("jk11m").className="fa fa-shopping-cart"
+  }
+
+rt1=!rt1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function ty311(){
+  let a= document.getElementById("id1aa");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id2aa");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty411(){
+
+
+   let a= document.getElementById("id1aa");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id2aa");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+
+function ty3331(){
+  let a= document.getElementById("id1111");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id21111");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty4441(){
+
+
+   let a= document.getElementById("id1111");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id21111");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+
+
+
+
+function ty311d(){
+  let a= document.getElementById("id1aad");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id2aad");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty411d(){
+
+
+   let a= document.getElementById("id1aad");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id2aad");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+
+function ty3331d(){
+  let a= document.getElementById("id1111d");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id21111d");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty4441d(){
+
+
+   let a= document.getElementById("id1111d");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id21111d");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ty31(){
+  let a= document.getElementById("id1a");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id2a");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty41(){
+
+
+   let a= document.getElementById("id1a");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id2a");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+function ty333(){
+  let a= document.getElementById("id111");
+  a.innerHTML=Number(a.innerHTML)+1
+ let m= document.getElementById("id211");
+  m.innerHTML=Number(m.innerHTML)+25.00
+}
+
+
+function ty444(){
+
+
+   let a= document.getElementById("id111");
+  a.innerHTML=Number(a.innerHTML)-1
+  let m= document.getElementById("id211");
+  m.innerHTML=Number(m.innerHTML)-25.00
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function kl(){
+ 
+  
+}
+
+
+
+function mgm(){
+  document.getElementById("bm").style="display:flex"
+    document.getElementById("bm1").style="display:none"
+  
+}
+function mgm1(){
+  document.getElementById("bm1").style="display:flex"
+    document.getElementById("bm").style="display:none"
+  
+}
+
+
+
+
+
+
+
+
+
+function omg(){
+
+document.getElementById("nbm").style="display:none"
+document.getElementById("olk").style="display:block"
+document.getElementById("okj").style="display:none"
+document.getElementById("okjl").style="display:none"
+document.getElementById("fgj").style="display:none"
+}
+
+
+function omg1(){
+
+
+document.getElementById("nbm").style="display:none"
+document.getElementById("olk").style="display:none"
+document.getElementById("okjl").style="display:none"
+document.getElementById("fgj").style="display:none"
+document.getElementById("okj").style="display:block"
+
+
+}
+
+function mgn(){
+
+document.getElementById("fgj").style="display:none"
+document.getElementById("nbm").style="display:none"
+document.getElementById("olk").style="display:none"
+document.getElementById("okj").style="display:none"
+document.getElementById("okjl").style="display:block"
+
+
+}
+
+function arb(){
+
+document.getElementById("nbm").style="display:none"
+document.getElementById("olk").style="display:none"
+document.getElementById("okj").style="display:none"
+document.getElementById("okjl").style="display:none"
+  document.getElementById("fgj").style="display:block"
+
+}
+
+
+
+
+let y=true
+function bvc44(){
+  if(y==true){
+  document.getElementById("bvc48id").style="transform:scale(1)"
+   document.getElementById("bvc44id").className="fa fa-arrow-up"
+  }
+  else{
+    document.getElementById("bvc48id").style="transform:scale(0)"
+   document.getElementById("bvc44id").className="fa fa-arrow-down"
+  }
+  y=!y
+}
+
+function bmw(){
+  y=!y
+document.getElementById("bvc42id").innerHTML="Sort by newness"
+ document.getElementById("bvc48id").style="transform:scale(0)"
+ document.getElementById("bvc44id").className="fa fa-arrow-down"
+}
+
+function bmw1(){ y=!y
+document.getElementById("bvc42id").innerHTML="Sort by popularity"
+ document.getElementById("bvc48id").style="transform:scale(0)"
+ document.getElementById("bvc44id").className="fa fa-arrow-down"
+}
+
+function bmw2(){y=!y
+document.getElementById("bvc42id").innerHTML="Sort by alphabet"
+ document.getElementById("bvc48id").style="transform:scale(0)"
+ document.getElementById("bvc44id").className="fa fa-arrow-down"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function rk(){
+  document.getElementById("pm").style="display:flex;"
+   document.getElementById("pm1").style="display:flex;"
+    document.getElementById("pm2").style="display:flex;"
+     document.getElementById("pm3").style="display:flex;"
+      document.getElementById("pm4").style="display:flex;"
+       document.getElementById("pm5").style="display:flex;"
+         document.getElementById("lmkn").style="display:block"
+           document.getElementById("lmka1").style="background:#c14679"
+            document.getElementById("lmkn2").style="color:#c14679"
+             document.getElementById("lmka2").style="background:#d7d7d7"
+            document.getElementById("lmkn3").style="color:black"
+               document.getElementById("lmka3").style="background:#d7d7d7"
+            document.getElementById("lmkn4").style="color:black"
+}
+
+
+
+function rka(){
+   document.getElementById("pm").style="display:flex;"
+   document.getElementById("pm1").style="display:flex;"
+    document.getElementById("pm2").style="display:none;"
+     document.getElementById("pm3").style="display:none;"
+      document.getElementById("pm4").style="display:flex;"
+       document.getElementById("pm5").style="display:none;"
+         document.getElementById("lmkn").style="display:flex"
+                   document.getElementById("lmka2").style="background:#c14679"
+            document.getElementById("lmkn3").style="color:#c14679"
+             document.getElementById("lmka1").style="background:#d7d7d7"
+            document.getElementById("lmkn2").style="color:black"
+               document.getElementById("lmka3").style="background:#d7d7d7"
+            document.getElementById("lmkn4").style="color:black"
+
+
+
+}
+
+
+function rkaa(){
+     document.getElementById("pm").style="display:none;"
+   document.getElementById("pm1").style="display:none;"
+    document.getElementById("pm2").style="display:flex;"
+     document.getElementById("pm3").style="display:flex;"
+      document.getElementById("pm4").style="display:none;"
+       document.getElementById("pm5").style="display:flex;"
+         document.getElementById("lmkn").style="display:flex"
+                    document.getElementById("lmka3").style="background:#c14679"
+            document.getElementById("lmkn4").style="color:#c14679"
+             document.getElementById("lmka2").style="background:#d7d7d7"
+            document.getElementById("lmkn3").style="color:black"
+               document.getElementById("lmka1").style="background:#d7d7d7"
+            document.getElementById("lmkn2").style="color:black"
+}
